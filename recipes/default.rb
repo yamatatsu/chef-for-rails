ad500ec54c0781e53b4f65be35820efbb5c3a6c4
yamatatsu <the.kinnikuman@gmail.com>
2015-12-10 17:08:25 +0900

    test code

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+# package 'nginx'
+#
+# service 'nginx' do
+#   action [:start]
+# end
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe 'chef-for-rails::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe service('nginx') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe port('80') do
+    it { should be_listening.with('tcp') }
   end
 end
